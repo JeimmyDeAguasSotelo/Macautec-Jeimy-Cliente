bc5af142e459838889e19d0e40ea59ab083ede10
JeimmtDeAguasSotelo <jsotelo@uninorte.edu.co>
2021-12-17 16:24:58 -0500

    create build

---
+++ build/precache-manifest.ea50dd609fcf319e339381cd5fa888e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c24c8f17f846564d7d3dc63f836622",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e02f6fa6333dc4b2201",
+    "url": "/static/css/2.fecfe2c5.chunk.css"
+  },
+  {
+    "revision": "c9e729ee5b6f49384817",
+    "url": "/static/css/main.b5e724a5.chunk.css"
+  },
+  {
+    "revision": "8e02f6fa6333dc4b2201",
+    "url": "/static/js/2.f3a23743.chunk.js"
+  },
+  {
+    "revision": "c9e729ee5b6f49384817",
+    "url": "/static/js/main.87d6f2a0.chunk.js"
+  },
+  {
+    "revision": "efe0184f84f7d13c34ec",
+    "url": "/static/js/runtime~main.04a84504.js"
+  }
+]);
